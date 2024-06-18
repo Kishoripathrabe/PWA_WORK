3d0b24bb5f85a60b17ff7720ac0804899c76b54d
“kishoripathrabe123@gmail.com” <kishoripathrabe123@gmail.com>
2024-06-18 18:58:54 +0530

    Scoping Read Out

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 if (navigator.serviceWorker) {  
     console.log("I am main.js")
-    navigator.serviceWorker.register('./sw.js').then((registration)=>{
+    navigator.serviceWorker.register('./sw.js',{scope:'/posts'}).then((registration)=>{
         console.log("sw registered");
     })
 }
-fetch('./camera.html').then((res)=>{
-    return res.text()
-}).then((html)=>{
-    document.getElementById('cam').innerHTML=html
-})
--- sw.js
+++ sw.js
@@ -1,14 +1,3 @@
 self.addEventListener('fetch',(e)=>{
-    if (e.request.url.endsWith('camera.html')) {
-        console.log("Fetch " + e.request.url)
-        e.respondWith(
-            fetch(e.request)
-            .then((res) => {
-                if(res.ok) {
-                    return res;
-                }
-                // fall back msg
-                return new Response('Image not available');
-        }))
-    }
+    console.log('fetch',e)
 })
