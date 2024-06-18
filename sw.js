b83d2f962e469d8beeda69697bf86d2dd5a4b336
“kishoripathrabe123@gmail.com” <kishoripathrabe123@gmail.com>
2024-06-18 14:29:00 +0530

    SW eventlisteners with self

--- sw.js
+++ sw.js
@@ -4,6 +4,12 @@ console.log("I am sw.js")
 
 
 self.addEventListener("install", (e) => {
+    let installPromise = new Promise(resolve => {
+        setTimeout(() => {
+            resolve("Completing Async Cache Task")
+        }, 5000)
+    })
+    e.waitUntil(installPromise);
     console.log("Sw:Install");
 })
 self.addEventListener("activate", (e) => {
