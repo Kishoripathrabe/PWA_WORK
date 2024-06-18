f14552475443633667a556ae1cb8259382ec79e9
“kishoripathrabe123@gmail.com” <kishoripathrabe123@gmail.com>
2024-06-18 14:28:01 +0530

    SW eventlisteners with self

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 
 console.log("I am sw.js")
 
+
+
+self.addEventListener("install", (e) => {
+    console.log("Sw:Install");
+})
+self.addEventListener("activate", (e) => {
+    console.log("Sw:Ativated");
+})
