34a26bfba4dcb7e6617760e2747ce9ea10273d72
“kishoripathrabe123@gmail.com” <kishoripathrabe123@gmail.com>
2024-06-18 18:16:20 +0530

    Fallback response for deletion file PRE

--- sw.js
+++ sw.js
@@ -1,2 +1,14 @@
 self.addEventListener('fetch',(e)=>{
+    if (e.request.url.endsWith('camera.html')) {
+        console.log("Fetch " + e.request.url)
+        e.respondWith(
+            fetch(e.request)
+            .then((res) => {
+                if(res.ok) {
+                    return res;
+                }
+                // fall back msg
+                return new Response('Image not available');
+        }))
+    }
 })
