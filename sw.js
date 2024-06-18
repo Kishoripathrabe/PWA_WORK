9994bc45999c5d4b8ee383c07593017f02b53022
“kishoripathrabe123@gmail.com” <kishoripathrabe123@gmail.com>
2024-06-18 17:32:58 +0530

    Intercepting the requests and mocking reponse

--- sw.js
+++ sw.js
@@ -1,18 +1,12 @@
-
-console.log("I am sw.js")
-
-
-
-self.addEventListener("install", (e) => {
-    let installPromise = new Promise(resolve => {
-        setTimeout(() => {
-            resolve("Completing Async Cache Task")
-        }, 5000)
-    })
-    e.waitUntil(installPromise);
-    self.skipWaiting()
-    console.log("Sw:Install");
-})
-self.addEventListener("activate", (e) => {
-    console.log("Sw:Ativated");
+self.addEventListener('fetch',(e)=>{
+    if(e.request.url.endsWith('style.css')){
+        console.log("Fetch " + e.request.url);
+        e.respondWith( fetch('style2.css'));
+    }
+    if(e.request.url.endsWith('greet')){
+        console.log("Fetch " + e.request.url)
+        let headers = new Headers({'Content-Type': 'text/html'});
+        let customRes = new Response('<h1>Greeting</h1>', { headers });
+        e.respondWith(customRes);
+    }
 })
