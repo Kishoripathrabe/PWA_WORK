986061819f28ef059ed02c5f2f5f1c24a6db4f49
“kishoripathrabe123@gmail.com” <kishoripathrabe123@gmail.com>
2024-06-19 12:50:35 +0530

    SW State Change Observed

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 if (navigator.serviceWorker) {  
     console.log("I am main.js")
-    navigator.serviceWorker.register('./sw.js',{scope:'/posts'}).then((registration)=>{
+    navigator.serviceWorker.register('./sw.js').then((registration)=>{
         console.log("sw registered");
-    })
+        registration.onupdatefound=()=>{
+            let newSW=registration.installing;
+            newSW.onstatechange=()=>{
+                console.log('current state',newSW.state)
+            }
+            console.log("new sw found");
+        }
+        
+})
 }
--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch',(e)=>{
-    console.log('fetch',e)
+self.addEventListener('install', (e) => {
+    console.log('installing');
+    e.waitUntil(new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("done");
+        },5000)
+    }));
+})
+self.addEventListener('activate', (e) => {
+    console.log('activated')
 })
